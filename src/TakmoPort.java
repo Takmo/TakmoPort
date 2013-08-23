c84b6bc36c02346072c65cd981d1f09ce2f81431
TechnoBulldog <techno.bulldog@gmail.com>
2013-08-22 21:11:54 -0500

    Initial commit.

---
+++ src/TakmoPort.java
@@ -0,0 +1,15 @@
+package com.bitwisehero.takmoport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TakmoPort extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
